5adc2afdc286bbc0acd4d9f41435b5f4eef02095
Maju Domenciano <juliadomenciano@hotmail.com>
2022-03-14 22:11:14 -0300

    (em andamento)

--- src/components/ProductsDetails.js
+++ src/components/ProductsDetails.js
@@ -7,9 +7,15 @@ class ProductsDetails extends React.Component {
     this.state = {
       email: '',
       menssage: '',
-      rating: [],
+      rating: 0,
       arr: [],
+      reviews: [],
     };
+  }
+
+  componentDidMount() {
+    this.loadReviews();
+    console.log(this.props.productId);
   }
 
 handleChange = ({ target }) => {
@@ -17,55 +23,83 @@ handleChange = ({ target }) => {
 }
 
 handleSubmit = () => {
-  const { productId } = this.props;
+  const { match: { params } } = this.props;
   const { email, menssage, rating, arr } = this.state;
-  this.setState({ email, menssage, rating });
   const rate = { email, menssage, rating };
-  this.setState((prevState) => ({
-    arr: [prevState.arr, rate],
-  }));
-  localStorage.setItem(productId, JSON.stringify(rate));
+  arr.push(rate);
+  localStorage.setItem(params.id, JSON.stringify(arr));
+  this.loadReviews();
+}
+
+loadReviews = () => {
+  const { match: { params } } = this.props;
+  const reviews = JSON.parse(localStorage.getItem(params.id));
+  if (reviews) {
+    this.setState({
+      reviews,
+    });
+  }
+
+  console.log(reviews);
 }
 
 render() {
-  const { email, menssage, rating } = this.state;
+  const { email, menssage, rating, reviews } = this.state;
   return (
-    <form>
-      <h2>Avaliações</h2>
-      <label htmlFor="product-detail-email">
-        <input
-          name="email"
-          type="email"
-          value={ email }
-          placeholder="Email"
-          data-testid="product-detail-email"
-          onChange={ this.handleChange }
-        />
-      </label>
-      <label htmlFor="rating">
-        Avalie de 1 a 5
-        <input
-          type="number"
-          name="rating"
-          value={ rating }
-        />
-      </label>
-      <label htmlFor="textArea">
-        <textarea
-          data-testid="product-detail-evaluation"
-          value={ menssage }
-          placeholder="Mensagem (opcional)"
-          onChange={ this.handleChange }
-        />
-      </label>
-      <button
-        type="button"
-        data-testid="submit-review-btn"
-        onClick={ this.handleSubmit }
-      >
-        Avaliar
-      </button>
-    </form>
+    <>
+      <form>
+        <h2>Avaliações</h2>
+        <label htmlFor="product-detail-email">
+          <input
+            name="email"
+            type="email"
+            value={ email }
+            placeholder="Email"
+            data-testid="product-detail-email"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="rating">
+          Avalie de 1 a 5
+          <input
+            type="number"
+            name="rating"
+            value={ rating }
+            onChange={ this.handleChange }
+          />
+        </label>
+        <label htmlFor="menssage">
+          <textarea
+            data-testid="product-detail-evaluation"
+            value={ menssage }
+            name="menssage"
+            placeholder="Mensagem (opcional)"
+            onChange={ this.handleChange }
+          />
+        </label>
+        <button
+          type="button"
+          data-testid="submit-review-btn"
+          onClick={ this.handleSubmit }
+        >
+          Avaliar
+        </button>
+      </form>
+      <section>
+        {!reviews
+          ? (
+            <p>
+              Avaliações
+            </p>)
+          : reviews.map((item, index) => (
+            <div key={ index }>
+              <p>{item.email}</p>
+              <p>{item.rating}</p>
+              <p>{item.menssage}</p>
+            </div>
+          ))}
+      </section>
+    </>
   );
 }
 }
